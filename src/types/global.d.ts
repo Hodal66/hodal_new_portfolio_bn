cbef2a108730d1f64748e127541dc32650f8977f
Hodal66 <mhthodol@gmail.com>
2026-03-22 03:18:12 +0200

    we have added the basic missing library

---
+++ src/types/global.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Global Type Definitions
+ * Use this file to declare modules that do not have official @types packages
+ * or to extend existing types.
+ */
+
+declare module 'morgan';
+declare module 'compression';
+declare module 'jsonwebtoken';
+declare module 'multer';
+declare module 'bcrypt';
+declare module 'validator';
+declare module 'nodemailer';
+declare module 'http-status';
+declare module 'express-rate-limit';
+declare module 'swagger-ui-express';
+declare module 'swagger-jsdoc';
